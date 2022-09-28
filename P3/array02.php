8e6d849242a44ade33c8686dfea807ac6de473cc
Muhammad Abbel Prasetya <114122498+abel027@users.noreply.github.com>
2022-09-28 22:50:43 +0700

    Create array02.php

---
+++ P3/array02.php
@@ -0,0 +1,12 @@
+<?php
+
+$arrNilai = array ("Ani" => 80,"Dani" => 70,"Eko" => 90);
+echo $arrNilai["Dani"]."<br>";
+
+$arrNilaiDua = array();
+$arrNilaiDua["Sulis"] = 80;
+$arrNilaiDua["Satria"] = 70;
+
+echo $arrNilaiDua["Sulis"];
+
+?>
