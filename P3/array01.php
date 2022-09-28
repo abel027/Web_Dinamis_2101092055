13b076d593d79bf9a254fc4008d48a2deca751dd
Muhammad Abbel Prasetya <114122498+abel027@users.noreply.github.com>
2022-09-28 22:50:46 +0700

    Create array01.php

---
+++ P3/array01.php
@@ -0,0 +1,15 @@
+<?php
+
+$arrBuah = array("Mangga","Apel","Pisang","Jeruk");
+echo $arrBuah[0]."<br>";
+echo $arrBuah[3]."<br>";
+
+$arrWarna = array();
+
+$arrWarna[]="Merah";
+$arrWarna[]="Biru";
+$arrWarna[]="Hijau";
+$arrWarna[]="Hitam";
+
+echo $arrWarna[2];
+?>
